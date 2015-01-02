e230a5288b3918eade7768dc7af412b32074744e
arumpuspa <aruma@outlook.co.id>
2015-01-02 20:40:58 +0700

    Moonphase fixed

---
+++ js/moonphase.js
@@ -0,0 +1,40 @@
+var lang  = 'en' // 'ca' , 'es' , 'en'
+var month = new Date().getMonth() + 1 // 1 - 12
+var year  = new Date().getFullYear()
+
+var svgSize        = 50 // pixels
+var svgLightColor  = "#FFFF88"
+var svgShadeColor  = "#111111"
+var svgSizeQuarter = 20 // pixels
+
+$(document).ready(function() {
+	sendAndRead()
+})
+
+function sendAndRead(){
+	$.ajax({
+		type: "GET",
+		url: "http://www.wdisseny.com/lluna/api/",
+		data: {
+			lang : lang,
+			size : svgSize,
+			lightColor : svgLightColor,
+			shadeColor : svgShadeColor,
+			sizeQuarter: svgSizeQuarter,
+			GTM : new Date().getTimezoneOffset(),
+			month :month,
+			year :year 
+		}
+	})
+	.done(function( response ) {
+		moon = $.parseJSON( response );
+		if (typeof example_1 == "function")example_1(moon)
+		if (typeof example_2 == "function")example_2(moon)
+		if (typeof example_3 == "function")example_3(moon)
+		$(".moon").css({backgroundColor:"#666666",display:"inline-table",padding:"10px 20px",color:"#FFFFFF",margin:"1px"})
+		$(".phaseLimit").css({position:"absolute",float:"left"})		
+	})
+	.fail(function(jqXHR, textStatus) {
+		$("#ex1").html(textStatus)
+	})
+}
